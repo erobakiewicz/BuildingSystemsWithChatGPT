bf27687024931e8af1d4acafe01fd5bb02d39178
erobakiewicz <eliasz.robakiewicz@netguru.com>
2023-08-09 14:57:55 +0200

    added moderation to filter out malicious and unacceptable questions and made instructions algorithmic

---
+++ list_of_products.py
@@ -0,0 +1,51 @@
+list_of_products = """
+1. Product: TechPro Ultrabook
+   Category: Computers and Laptops
+   Brand: TechPro
+   Model Number: TP-UB100
+   Warranty: 1 year
+   Rating: 4.5
+   Features: 13.3-inch display, 8GB RAM, 256GB SSD, Intel Core i5 processor
+   Description: A sleek and lightweight ultrabook for everyday use.
+   Price: $799.99
+
+2. Product: BlueWave Gaming Laptop
+   Category: Computers and Laptops
+   Brand: BlueWave
+   Model Number: BW-GL200
+   Warranty: 2 years
+   Rating: 4.7
+   Features: 15.6-inch display, 16GB RAM, 512GB SSD, NVIDIA GeForce RTX 3060
+   Description: A high-performance gaming laptop for an immersive experience.
+   Price: $1199.99
+
+3. Product: PowerLite Convertible
+   Category: Computers and Laptops
+   Brand: PowerLite
+   Model Number: PL-CV300
+   Warranty: 1 year
+   Rating: 4.3
+   Features: 14-inch touchscreen, 8GB RAM, 256GB SSD, 360-degree hinge
+   Description: A versatile convertible laptop with a responsive touchscreen.
+   Price: $699.99
+
+4. Product: TechPro Desktop
+   Category: Computers and Laptops
+   Brand: TechPro
+   Model Number: TP-DT500
+   Warranty: 1 year
+   Rating: 4.4
+   Features: Intel Core i7 processor, 16GB RAM, 1TB HDD, NVIDIA GeForce GTX 1660
+   Description: A powerful desktop computer for work and play.
+   Price: $999.99
+
+5. Product: BlueWave Chromebook
+   Category: Computers and Laptops
+   Brand: BlueWave
+   Model Number: BW-CB100
+   Warranty: 1 year
+   Rating: 4.1
+   Features: 11.6-inch display, 4GB RAM, 32GB eMMC, Chrome OS
+   Description: A compact and affordable Chromebook for everyday tasks.
+   Price: $249.99
+"""
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import openai
 import tiktoken
 from dotenv import load_dotenv, find_dotenv
 
+from list_of_products import list_of_products
+
 _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -14,16 +16,26 @@ The messages will be provided in the following format:
 Customer: Hello, I have a problem with my order.
 {delimiter}
 
-Classify the message as one of the following categories billing, support, general inquiry:
-1. Billing: unsubscribe, upgrade
-2. Support: technical, bug, issue
-3. Account Management: login, password change, deactivate account
-4. General Inquiry: question, inquiry, other
+To answer perform following steps:
+1. Classify the message as one of the following categories billing, support, general inquiry:
+    1. Billing: unsubscribe, upgrade
+    2. Support: technical, bug, issue
+    3. Account Management: login, password change, deactivate account
+    4. General Inquiry: question, inquiry, other
+2. Check if the message is about a product from a list of products {list_of_products}. 
+If not ask about product which might be related to the message.
+3. If the message is about a product list all assumptions the user is making about the product in the message.
+4. Check if those assumptions are correct.
+RESPONSE TO USER: Formulate response for a user based on the message and the assumptions. 
+Try to correct politely any incorrect assumptions.
 
 Provide your response in the following format:
-{delimiter}
-Service: Hello, I am here to help you with your order.
-{delimiter}
+Use the following format:
+Step 1:{delimiter} <step 1 reasoning>
+Step 2:{delimiter} <step 2 reasoning>
+Step 3:{delimiter} <step 3 reasoning>
+Step 4:{delimiter} <step 4 reasoning>
+Response to user:{delimiter} <response to user>
 """
 
 
@@ -31,12 +43,21 @@ def get_moderation_pass(msg):
     return openai.Moderation.create(input=msg)["results"][0]['flagged']
 
 
+def get_only_final_answer(msg):
+    try:
+        final_response = msg.split(delimiter)[-1].strip()
+    except Exception as e:
+        final_response = "Sorry, I'm having trouble right now, please try asking another question."
+
+    return final_response
+
+
 def get_completion_from_messages(messages,
                                  model="gpt-3.5-turbo",
                                  temperature=0,
                                  max_tokens=500):
-    moderation_pass = get_moderation_pass(messages)
-    if moderation_pass:
+    moderation_flagged = get_moderation_pass(messages)
+    if not moderation_flagged:
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
@@ -50,4 +71,4 @@ def get_completion_from_messages(messages,
     return {"error": "message failed moderation"}, 400
 
 
-print(get_completion_from_messages("prompt"))
+print(get_completion_from_messages("What is more expensive BlueWave laptop or BlackAddler nanobook?"))
