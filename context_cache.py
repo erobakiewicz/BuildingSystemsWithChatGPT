3891f1e1fee8cb4f9d2006afd12c7201f545b6d1
erobakiewicz <eliasz.robakiewicz@netguru.com>
2023-08-11 18:18:09 +0200

    added documentation

--- context_cache.py
+++ context_cache.py
@@ -5,6 +5,13 @@ context_cache = client.chatbot.context_cache.users
 
 
 def make_context(user_name, message, answer):
+    """
+    Creates or updates a context for a specific user by storing in db the message and answers.
+    :param user_name: user we want to make the context for
+    :param message: message to be stored
+    :param answer: answer to be stored
+    :return: None
+    """
     if not context_cache.find_one({"user_name": user_name}):
         context_cache.insert_one(
             {
@@ -19,6 +26,11 @@ def make_context(user_name, message, answer):
 
 
 def get_context(user_name):
+    """
+    Gets the context from the database for a specific user
+    :param user_name: user we want to retrieve the context for
+    :return: string representation of a list of messages and answers
+    """
     context = context_cache.find_one({"user_name": user_name})
     if context:
         return format_context(context.get("messages"))
